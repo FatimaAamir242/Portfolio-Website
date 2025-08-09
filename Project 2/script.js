904c4269f653bc1e6a810012cdcf0e1a9f0bb705
FatimaAamir242 <famir9588@gmail.com>
2025-08-09 23:16:38 +0500

    commit

---
+++ Project 2/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+const hamburger=document.getElementById("hamburger")
+const list=document.getElementById("list")
+
+ hamburger.addEventListener("click",()=>{
+   list.classList.toggle("show"); 
+ }) 
+ }); 
